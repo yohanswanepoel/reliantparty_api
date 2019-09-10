5a75d6093c628fd6868b05851de1a55e2058b2b1
Johan Swanepoel <johannes.swanepoel@gmail.com>
2019-09-10 10:58:28 +1000

    Adding some tests

--- reliantparty/tests.py
+++ reliantparty/tests.py
@@ -13,10 +13,63 @@ class BaseViewTest(APITestCase):
     def create_reliant_party(name="", email="", abn="", status=""):
         if name != "" and email != "" and abn != "" and status != "":
             ReliantParty.objects.create(name=name, email=email, abn=abn, status=status)
-        
+    
+    def make_a_request(self, kind="post", **kwargs):
+        """
+        Make a post request to create a song
+        :param kind: HTTP VERB
+        :return:
+        """
+        if kind == "post":
+            return self.client.post(
+                reverse(
+                    "reliantparties-list-create",
+                    kwargs={
+                        "version": kwargs["version"]
+                    }
+                ),
+                data=json.dumps(kwargs["data"]),
+                content_type='application/json'
+            )
+        elif kind == "put":
+            return self.client.put(
+                reverse(
+                    "reliantparties-detail",
+                    kwargs={
+                        "version": kwargs["version"],
+                        "pk": kwargs["id"]
+                    }
+                ),
+                data=json.dumps(kwargs["data"]),
+                content_type='application/json'
+            )
+        else:
+            return None
+
+    def delete_a_song(self, pk=0):
+        return self.client.delete(
+            reverse(
+                "reliantparties-detail",
+                kwargs={
+                    "version": "v1",
+                    "pk": pk
+                }
+            )
+        )
+
     def setUp(self):
         self.create_reliant_party("Name1","name@email.com","some abn","active")
         self.create_reliant_party("Name2","name@email.com","some abn 2","testing")
+        self.valid_data = {
+            "name": "test song",
+            "status": "test artist",
+            "email": "test song",
+            "abn": "test artist"
+        }
+        self.invalid_data = {
+            "name": "test song",
+            "abn": "test artist"
+        }
 
 class GetAllReliantPartiesTest(BaseViewTest):
 
@@ -28,3 +81,21 @@ class GetAllReliantPartiesTest(BaseViewTest):
         serialized = ReliantPartySerializer(expected, many=True)
         self.assertEqual(response.data, serialized.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class AddReliantPartyTest(BaseViewTest):
+
+    def test_create_reliant_party(self):
+        response = self.make_a_request(
+            "kind"="post",
+            "version"="v1",
+            "data"=self.valid_data
+        )
+        self.assertEqual(response.data, self.valid_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # test with invalid data
+        response = self.make_a_request(
+            kind="post",
+            version="v1",
+            data=self.invalid_data
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
